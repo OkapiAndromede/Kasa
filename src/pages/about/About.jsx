9dda9921157b540b223d17a659a7b9b32a06706c
Neal Muller <neal.henri.muller@gmail.com>
2025-07-03 11:41:26 +0200

    Ajout du composant Banner dans la page à propos

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,9 +1,13 @@
 import Navigation from "../../components/navBar/Navigation";
+import Banner from "../../components/banner/Banner";
+
+import aboutImg from "../../assets/image_about.svg";
 
 function About() {
   return (
     <>
       <Navigation></Navigation>
+      <Banner text={""} backgroundImage={aboutImg}></Banner>
       <h1>Page à propos</h1>
     </>
   );
