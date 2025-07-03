c3eed8ee7a24d0d64bade989e4ae7429257852f8
Neal Muller <neal.henri.muller@gmail.com>
2025-07-03 09:35:48 +0200

    Correction des marges et ajout du composant footer

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import Banner from "../../components/banner/Banner";
 import Navigation from "../../components/navBar/Navigation";
 import Gallery from "../../components/gallery/Gallery";
+import Footer from "../../components/footer/Footer";
 
 function Home() {
   return (
@@ -8,7 +9,7 @@ function Home() {
       <Navigation></Navigation>
       <Banner></Banner>
       <Gallery></Gallery>
-      <h1>Page d'accueil</h1>
+      <Footer></Footer>
     </>
   );
 }
