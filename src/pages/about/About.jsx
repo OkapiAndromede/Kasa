d22ecc64c525f467a42e1b379865ba5d99ac2c91
Neal Muller <neal.henri.muller@gmail.com>
2025-07-04 11:58:18 +0200

    Ajout du composant Collapse et gestion de la prise d'espace du corps/footer

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -2,13 +2,16 @@ import Navigation from "../../components/navBar/Navigation";
 import Banner from "../../components/banner/Banner";
 
 import aboutImg from "../../assets/image_about.svg";
+import Footer from "../../components/footer/Footer";
+import Collapse from "../../components/collapse/Collapse";
 
 function About() {
   return (
     <>
       <Navigation></Navigation>
       <Banner text={""} backgroundImage={aboutImg}></Banner>
-      <h1>Page à propos</h1>
+      <Collapse></Collapse>
+      <Footer></Footer>
     </>
   );
 }
