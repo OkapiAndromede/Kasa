b5c3968669a1f94bd52a8d86f6a474fc44ccc4b2
Neal Muller <neal.henri.muller@gmail.com>
2025-07-09 11:39:05 +0200

    Animation 100% fonctionnelle

--- src/components/collapse/Collapse.jsx
+++ src/components/collapse/Collapse.jsx
@@ -51,6 +51,7 @@ function Wrapper({ title, children }) {
             viewBox="0 0 25 14"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
+            className={isActive ? "rotated" : ""}
           >
             <path
               d="M11.2897 0.789661C11.9591 0.120242 13.0462 0.120242 13.7157 0.789661L23.9979 11.0719C24.6674 11.7414 24.6674 12.8285 23.9979 13.4979C23.3285 14.1673 22.2414 14.1673 21.572 13.4979L12.5 4.42595L3.42804 13.4926C2.75862 14.162 1.67148 14.162 1.00206 13.4926C0.332646 12.8231 0.332646 11.736 1.00206 11.0666L11.2843 0.784306L11.2897 0.789661Z"
@@ -59,13 +60,10 @@ function Wrapper({ title, children }) {
           </svg>
         </div>
       </div>
-      {isActive ? (
-        <div className="quality__item--content">
-          <p>{children}</p>
-        </div>
-      ) : (
-        ""
-      )}
+
+      <div className={`quality__item--content ${isActive ? "active" : ""}`}>
+        <p>{children}</p>
+      </div>
     </article>
   );
 }
