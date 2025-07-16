61019a4aec3d762687dcdbecf06190793376bc1b
Neal Muller <neal.henri.muller@gmail.com>
2025-07-16 16:51:06 +0200

    Optimisation du responsive

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,11 +1,11 @@
 import "./banner.scss";
-function Banner({ text, backgroundImage }) {
+function Banner({ text, backgroundImage, className }) {
   return (
     <div
       className="heroBanner"
       style={{ backgroundImage: `url(${backgroundImage})` }}
     >
-      <div className="heroBanner__box">
+      <div className={`heroBanner__box ${className}`}>
         <p className="heroBanner__txt"> {text} </p>
       </div>
     </div>
